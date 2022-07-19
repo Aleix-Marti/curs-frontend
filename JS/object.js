1e216104c1bab344c9acafbf9b2ca5b776447c92
Aleix-Marti <aleix.marti@gmail.com>
2022-07-19 09:41:29 +0200

    exemples d'objectes

--- JS/object.js
+++ JS/object.js
@@ -1,5 +1,6 @@
 const out = document.getElementById('info');
 
+// objecte creat directament
 const simpson = {
   name: 'Bart',
   age: 10,
@@ -13,30 +14,55 @@ console.dir(simpson);
 let txt = "";
 
 for (let x in simpson) {
-    txt += simpson[x] + " ";
+    txt += simpson[x] + " "; // és el mateis que: txt = txt + simpson[x] + " ";
     console.log(txt);
     console.log(x);
-    // txt = txt + simpson[x] + " ";
 };
 
 out.innerHTML = txt;
 
 
-
+// es pot convertir objecte en array
 const myArray = Object.values(simpson);
 out.innerHTML = myArray;
 
 
-
+// es pot crear un constructor d'objectes genèric
 function Simpson(name, age, haircolor){
+  // conté informació
   this.name = name;
   this.age = age;
   this.hairColor = haircolor;
+  // conté un mètode que, en aquest cas retorna una frase amb tota la informació
   this.hello = function(){
     return `El personatge ${this.name} té ${this.age} anys i el cabell de color ${this.hairColor}`;
   }
 }
+// es poden crear tants objectes com es vulguin utilitzant el constructor.
 const bart = new Simpson('Bart',10,'yellow');
 const marge = new Simpson('Marge',36,'blue');
 
-console.log(bart.hello());
+// per mostrar informació, es crida la clau
+console.log( bart.name );
+// per invocar el mètode, s'ha de cridar amb ()
+console.log( bart.hello() );
+
+
+
+
+
+
+// exemple d'una funció que demana informació, crea un objecte a través del constructor creat anteriorment
+// i mostra la informació amb el mètode propi de l'objecte
+function demana(){
+  
+  let nom = prompt('Entra el nom');
+  let edat = prompt('Entra edat');
+  let cabell = prompt('Entra el color de cabell');
+
+  let newperson = new Simpson(nom,edat,cabell);
+
+  alert( newperson.hello() );
+}
+
+demana();
