969904c2eeb7b7f2d775b537004c42d36dd90203
it-human <it@humancta.org>
2022-09-30 21:29:57 +0200

    projecte app Vue

---
+++ Vue/app-vue/src/views/NewView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import HelloWorld from '../components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="new">
+    <h1>This is our new page</h1>
+    <h2>This is our subtitle</h2>
+    <HelloWorld msg="You did it again!!" /> 
+  </div>
+</template>
+
+<style>
+.new {
+  display: grid;
+}
+@media (min-width: 1024px) {
+  .new {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
